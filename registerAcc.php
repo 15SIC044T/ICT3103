185c707e1599ecf81d9d0f57f7abf6834a845650
mhpek <menghwee.pek_2015@sit.singaporetech.edu.sg>
2017-09-29 01:12:35 +0800

    Updated index.php, registerAcc.php

--- registerAcc.php
+++ registerAcc.php
@@ -14,8 +14,6 @@
                 <div class="col-sm-12 registerblock">
                     <div>
                         <form action="checklogin.php" method="post">
-<<<<<<< HEAD
-=======
                             <?php
                             //Display error message if the login failed
                             if (!empty($_SESSION['login_error_msg'])) {
@@ -24,26 +22,21 @@
                             }
                             ?>
 
->>>>>>> 7579fd02010020a2f374cf88b9ed9fb104fd59c3
                             <input name="user" type="text" class="form-control" placeholder="Username" required autofocus>
 
                             <input name="password" type="password" class="form-control" placeholder="Password" required>
-                            
-<<<<<<< HEAD
+
                             <input name="email" type="text" class="form-control" placeholder="Email Address" required>
-=======
-                            <input name="email" type="text" class="form-control" placeholder="Email" required>
->>>>>>> 7579fd02010020a2f374cf88b9ed9fb104fd59c3
-                            
+
                             <input name="mobileNumber" type="text" class="form-control" placeholder="Mobile Number" required>
 
                             <button class="btn btn-lg btn-block" name="register" type="submit">Sign Up</button>
                         </form>
                     </div>
-                    
+
                     <div class="col-sm-12 centerblock">
                         <div class="row">
-                                Have an account? <a href="index.php">Sign In!</a>
+                            Have an account? <a href="index.php">Sign In!</a>
                         </div>
                     </div>
                 </div>
