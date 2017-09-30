8fa057d2c1e0c2ce5993a83f63b220866533b61e
mhpek <menghwee.pek_2015@sit.singaporetech.edu.sg>
2017-09-30 20:51:37 +0800

    [working in progress] sendResetPasswordEmail.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php include "header.php" ?>
-    <?php include "resetPassword.php" ?>
+    <?php include "resetPasswordModal.php" ?>
 
     <body>
         <div class="container-fluid">
--- sendResetPasswordEmail.php
+++ sendResetPasswordEmail.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 include 'db-connection.php';
 include 'emailFunction.php';
@@ -8,7 +8,7 @@ $email = $_POST['email'];
 $connection = new Mysql_Driver();
 $connection->connect();
 
-echo "$email";
+echo $email ."- this .<br>";
 
 // create query
 $qry = "SELECT * FROM account WHERE email ='$email'";
@@ -22,15 +22,21 @@ if ($connection->num_rows($result) == 1) {
     // email content
     $subject = "Password Recovery";
     $message = "
-       Hello," . $_SESSION['SESS_USERNAME'] . ",
-       <p>We received a request to reset your password for DropIT Sharing:</p>" . $email . "
-       <a href='http://localhost/ICT3103/confirmPasswordResetPage.php?id='" . $_SESSION['SESS_ACC_ID'] . "'>Click the button to reset your password</a></p>
+       Hello, " . $_SESSION['SESS_USERNAME'] . ",
+       <p>We received a request to reset your password for DropIT Sharing: <strong>" . $email . "</strong></p>
+       <p><a href='http://localhost/ICT3103/confirmPasswordReset.php?id=".$_SESSION['SESS_ACC_ID']."'>Click the button to reset your password</a></p>
        <p>Please ignore this message if you didn't ask to change your password.</p>";
 
     send_mail($subject, $email, $message);
     
     // redirect 
-    header("Location: index.php");
+    //header("Location: file.php");
+    
+    echo $_SESSION['SESS_ACC_ID'] ."<br>";
+    echo $_SESSION['SESS_USERNAME'] ."<br>";
+    echo $subject ."<br>";
+    echo $message ."<br>";
+    echo "<br><br><br>";
 } else {
     
 }
