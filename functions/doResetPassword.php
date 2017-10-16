988337c7de5c1884e30a53edad5e08d3cf5f9df2
mhpek <menghwee.pek_2015@sit.singaporetech.edu.sg>
2017-10-16 17:10:14 +0800

    [update] change sha to bcrypt

--- functions/doResetPassword.php
+++ functions/doResetPassword.php
@@ -8,8 +8,8 @@ include '../db-connection.php';
 
 // sanitize the POST values
 $userId = $_SESSION['SESS_ACC_ID'];
-$password = sha1($_POST['inputPass']);
-$confirmPassword = sha1($_POST['inputConfirmPass']);
+$password = $_POST['inputPass'];
+$confirmPassword = $_POST['inputConfirmPass'];
 
 // connect database
 $connection = new Mysql_Driver();
@@ -19,15 +19,16 @@ $connection->connect();
 if ($password != $confirmPassword) {
     header("Location: ../confirmPasswordReset.php?id=$getUserId");
     $_SESSION['error_msg'] = "Password not the same!";
-    exit();
 } else {
+    // password hashing
+    $confirmPassHash = password_hash($confirmPassword, PASSWORD_BCRYPT);
+
     $queryUpdate = "UPDATE account 
-                    SET password = '$confirmPassword' 
+                    SET password = '$confirmPassHash' 
                     WHERE accountID = $userId";
     $updateDB = $connection->query($queryUpdate);
 
     header("Location: ../index.php");
-    $_SESSION['success_msg'] = "Password Changed!";
-    exit();
+    $_SESSION['success_msg'] = "Password changed!";
 }
 ?>
